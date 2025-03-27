d91122892c215d9c18f49a33753cb9919b4ab867
Eric Sayer <me@ericsayer.com>
2025-03-26 18:45:06 -0600

    tests passing

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -15,18 +15,19 @@ export default function Calculator() {
 
   return (
     <div className="calculator">
-      <p>
-        <input
-          type="text"
-          id="value"
-          name="value"
-          onChange={(event) => { setValue(Number(event.target.value)); }}
-          value={value}
-        />
-      </p>
-      <button className="btn btn-primary" type="button" onClick={handleAddClick}> Add </button>
-      <p className="card-text">Total: {calcState.total}</p>
-      <p className="card-text">Previous Total: {calcState.previousTotal}</p>
+      <h2>Simple Calculator</h2>
+        <p>
+          <input
+            type="text"
+            id="value"
+            name="value"
+            onChange={(event) => { setValue(Number(event.target.value)); }}
+            value={value}
+          />
+        </p>
+        <button className="btn btn-primary" type="button" onClick={handleAddClick}> Add </button>
+        <p className="card-text">Total: {calcState.total}</p>
+        <p className="card-text">Previous Total: {calcState.previousTotal}</p>
     </div>
   );
 }
